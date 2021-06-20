cc80594417a95dc9093b4bcf97b9eb85c12be0da
hlkirkness <hlkirkness@gmail.com>
2021-06-20 19:58:09 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var starBtn = document.querySelector("#startBtn");
+var questionsDiv = document.querySelector("#questions");
+var introDiv = document.querySelector("#intro");
+var btnEl = document.querySelector("#my-btn");
+var questionEl = document.querySelector("#show-q");
+var timeRemaining = question.length * 10;
+var questions = [
+    {
+        q: 'Commonly used data types DO NOT include',
+        c: ['1. Strings','2. Booleans','3. Alerts','4. Numbers'],
+        a: 3
+    },
+    {
+        q: 'The condition in an if/else statement is enclosed with _____',
+        c: ['1. Quotes','2. Curly Brackets','3. Parentheses','4. Square Brackets'],
+        a: 2
+    }
+]
