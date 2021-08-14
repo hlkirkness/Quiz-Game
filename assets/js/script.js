aa6f2b8ad343a8fb467ae6d4d5d3c0bea17f0fdf
hlkirkness <hlkirkness@gmail.com>
2021-08-14 13:08:33 -0400

    if statement fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,12 +20,12 @@ document.addEventListener('DOMContentLoaded', () => {
     {
       q: 'Commonly used data types DO NOT include',
       c: ['1. Strings', '2. Booleans', '3. Alerts', '4. Numbers'],
-      a: 3
+      a: 2
     },
     {
       q: 'The condition in an if/else statement is enclosed with _____',
       c: ['1. Quotes', '2. Curly Brackets', '3. Parentheses', '4. Square Brackets'],
-      a: 2
+      a: 1
     },
     {
       q: 'Arrays in JavaScript can be used to store _____',
@@ -109,82 +109,84 @@ document.addEventListener('DOMContentLoaded', () => {
 }
 
 choice1.addEventListener("click", function(){
- questionIndex++
- loadQuestion()
+ onChoiceClicked(0)
 })
 choice2.addEventListener("click", function(){
- questionIndex++
- loadQuestion()
+ onChoiceClicked(1)
  })
  choice3.addEventListener("click", function(){
-   questionIndex++
-   loadQuestion()
+   onChoiceClicked(2)
  })
  choice4.addEventListener("click", function(){
-   questionIndex++
-   loadQuestion()
+   onChoiceClicked(3)
  })
 
-if (questions[questionIndex].c != questions[questionIndex].a) {
-    (timeLeft-10)
-}
-
-
-
-})
-// var startBtn = document.querySelector("#startBtn")
-// var TimeLeftSpan= document.querySelector("#TimeLeftSpan")
-var initialDiv = document.querySelector("#initialDiv")
-
-var timeLeft = questions.length * 10
-
-var questionIndex = 0
-
-var pidClock
-
-startBtn.addEventListener("click", function () {
-  questionsDiv.classList.remove("hide")
-  introDiv.classList.add("hide")
-  loadQuestion()
-  pidClock = setInterval(countDown, 1000)
-
-})
-function loadQuestion() {
-
-  title.textContent = questions[questionIndex].q
-  choice1.textContent = questions[questionIndex].c[0]
-  choice2.textContent = questions[questionIndex].c[1]
-  choice3.textContent = questions[questionIndex].c[2]
-  choice4.textContent = questions[questionIndex].c[3]
-
-
-}
-
-choice1.addEventListener("click", function () {
-  questionIndex++
-  loadQuestion()
-})
-choice2.addEventListener("click", function () {
-  questionIndex++
-  loadQuestion()
-})
-choice3.addEventListener("click", function () {
-  questionIndex++
-  loadQuestion()
-})
-choice4.addEventListener("click", function () {
-  questionIndex++
-  loadQuestion()
-})
-function countDown() {
-  if (timeLeft === 0) {
-    clearInterval(pidClock)
-    questionsDiv.classList.add("hide")  //adding the hide class to make it invisible
-    initialDiv.classList.remove("hide") // removing the hide class to make it visible
-
+ function onChoiceClicked(choiceIndex) {
+  if (choiceIndex != questions[questionIndex].a) {
+      (timeLeft-=10)
   }
-  TimeLeftSpan.textContent = timeLeft
-  timeLeft--  // timeRemaining = timeRemaining-1
+  questionIndex++
+  loadQuestion()
+ }
 
 
-}
+
+
+
+ })
+// // var startBtn = document.querySelector("#startBtn")
+// // var TimeLeftSpan= document.querySelector("#TimeLeftSpan")
+// var initialDiv = document.querySelector("#initialDiv")
+
+// var timeLeft = questions.length * 10
+
+// var questionIndex = 0
+
+// var pidClock
+
+// startBtn.addEventListener("click", function () {
+//   questionsDiv.classList.remove("hide")
+//   introDiv.classList.add("hide")
+//   loadQuestion()
+//   pidClock = setInterval(countDown, 1000)
+
+// })
+// function loadQuestion() {
+
+//   title.textContent = questions[questionIndex].q
+//   choice1.textContent = questions[questionIndex].c[0]
+//   choice2.textContent = questions[questionIndex].c[1]
+//   choice3.textContent = questions[questionIndex].c[2]
+//   choice4.textContent = questions[questionIndex].c[3]
+
+
+// }
+
+// choice1.addEventListener("click", function () {
+//   questionIndex++
+//   loadQuestion()
+// })
+// choice2.addEventListener("click", function () {
+//   questionIndex++
+//   loadQuestion()
+// })
+// choice3.addEventListener("click", function () {
+//   questionIndex++
+//   loadQuestion()
+// })
+// choice4.addEventListener("click", function () {
+//   questionIndex++
+//   loadQuestion()
+// })
+// function countDown() {
+//   if (timeLeft === 0) {
+//     clearInterval(pidClock)
+//     questionsDiv.classList.add("hide")  //adding the hide class to make it invisible
+//     initialDiv.classList.remove("hide") // removing the hide class to make it visible
+
+//   }
+//   TimeLeftSpan.textContent = timeLeft
+//   timeLeft--  // timeRemaining = timeRemaining-1
+
+
+// }
